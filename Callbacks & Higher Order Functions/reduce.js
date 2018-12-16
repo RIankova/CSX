7b90fb957dfc9ffaa29b7217cfc33631cc056926
Ralitza Iankova <iankova.ralitza@gmail.com>
2018-12-16 09:03:56 -0800

    complete reduce assignment

--- Callbacks & Higher Order Functions/reduce.js
+++ Callbacks & Higher Order Functions/reduce.js
@@ -1,28 +1,66 @@
-// the function `reduce` takes an array and reduces the elements to a single value
-// the `reduce` function loops through the array and applies any operation that you can put into a function to each element in the array while keeping track of the outcome of each loop.
-  // in this way, we could use `reduce` to do things like sum all the numbers in an array or multiply them all together
-  /*
-  var nums = [4, 1, 3];
-  var add = function(a, b) {return a + b}
-  reduce(nums, add, 0) // -> 8
-  */
+// reduce() examples:
+// let arr = [1, 2, 3];
 
-// define a 'reduce' function that accepts:
-  // an array
-  // a callback
-  // an initial value and returns a single value
+// let total = arr.reduce(function(total, num) {
+//   // total is the accumulator
+//   // -> it keeps track of the outcome of each loop
+//   return total + num;
+// }, 0);
 
-function reduce(array, callback, initialValue) {
-  let sum = initialValue;
-  for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
+// console.log(total); // -> 6
+
+// // using ES6 arrow function syntax
+// // arr.reduce((total, num) => total + num, 0)
+
+// var nums = [4, 1, 3];
+
+// console.log(sum);
+// // the 'reduce' function loops through the array and applies any operation that you can put into a function to each element in the array while keeping track of the outcome of each loop. In this way, we could use 'reduce' to do things like sum all the numbers in an array or multiply them all together
+
+// input: arr, callback, initial value
+// output: single value
+
+// my attempt
+function reduce(arr, callback, initialValue) {
+  // ensure argument is an array
+  if (Array.isArray(arr)) {
+    // assign accumulator to initialValue or assign to 0 if undefined
+    let total = initialValue || 0;
+    // iterate over array and run callback while passing in the accumulator value and the current array element
+    for (let i = 0; i < arr.length; i++) {
+      // assign accumulator to the callback's return value
+      total = callback(total, arr[i]);
+    }
+    return total;
   }
-
+  return 'Please enter an array';
 }
 
-function add(num) {
-  return num + num;
+function multiplyBy2(total, number) {
+  return total * number;
 }
 
-var nums = [4, 1, 3];
-console.log(reduce(nums, add, 0)); // should log 8
+console.log(reduce('[1, 2, 3]', multiplyBy2, 2)); // -> 12
+
+//---------------
+
+// solution video
+
+function reduce2(array, callback, initial) {
+  if (Array.isArray(array)) {
+    let acc;
+    if (initial === undefined) {
+      acc = array[0];
+      array = array.slice(1);
+    } else {
+      acc = initial;
+    }
+    array.forEach(function (element) {
+      acc = callback(acc, element);
+    });
+    return acc;
+  }
+  return 'The first argument should be an array';
+}
+
+console.log(reduce2('3', function (total, number) { return total * number;}, 2)); // -> 12
